a0b426610b54e6997d06dee6fa761c67cc137be0
cj.huang <cj.huang@Ctrip.com>
2020-02-17 10:15:59 +0800

    动态规划：凑硬币

---
+++ src/main/java/com/study/CoinChange.java
@@ -0,0 +1,31 @@
+package com.study;
+
+/**
+ * 硬币  2  5  7  用最少的硬币数拼出27
+ */
+public class CoinChange {
+    public int coinChange (int[] A, int M) {
+        int n = A.length;
+        //表示拼出下标i的最少硬币数为f[i]
+        int[] f = new int[M + 1];
+        f[0] = 0;
+
+        for (int i = 1; i < f.length; ++i) {
+            f[i] = Integer.MAX_VALUE;
+            for(int k = 0; k < n; k ++) {
+                if(i >= A[k] && f[i - A[k]] != Integer.MAX_VALUE) {
+                    f[i] = Math.min(f[i], f[i - A[k]] + 1);
+                }
+            }
+            System.out.println("f[" + i + "]=" + f[i]);
+        }
+        if(f[M] == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return f[M];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new CoinChange().coinChange(new int[]{2, 5, 7}, 27));
+    }
+}
